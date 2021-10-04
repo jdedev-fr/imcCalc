ebc1372bfffa58a2eaa62975a1714dfef804f179
JdeDev <julien@jdedev.fr>
2021-10-04 10:58:48 +0200

    ajout commentaires

--- js/calcimc.js
+++ js/calcimc.js
@@ -1,10 +1,16 @@
+// Fonction appellée par le formulaire
 function calculImc(e) {
+    //On empeche l'envois réel du formulaire et donc le refresh de la page
     e.preventDefault()
+
+    //On récupère les valeurs de poids et de taille
     let poids = document.getElementById('poids').value
     let taille = document.getElementById('taille').value
 
+    //On calcule l'IMC
     let imc = poids / (taille / 100 * taille / 100)
 
+    //On mets en forme la couleur de fond du résultat pour plus de lisibilitée
     if (imc < 18.5) {
         document.querySelector('.res').style.backgroundColor = "rgb(97, 201, 219)"
     } else if (imc < 25) {
@@ -19,5 +25,7 @@ function calculImc(e) {
         document.querySelector('.res').style.backgroundColor = "rgb(95, 15, 5)"
         document.querySelector('.res').style.color = "white"
     }
+
+    //On mets le message de résultat
     document.querySelector('.res').innerHTML = "Votre IMC est de : " + parseFloat(imc).toFixed(2)
 }
